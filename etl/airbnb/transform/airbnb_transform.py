dfa6b9b17a18c49f344d1767dc1a95ca9b047393
Vinícius Vicari <vikarih.punk@gmail.com>
2025-03-26 17:40:04 -0300

    adding last changes of airbnb etl

--- etl/airbnb/transform/airbnb_transform.py
+++ etl/airbnb/transform/airbnb_transform.py
@@ -34,10 +34,9 @@ def transform_listings_silver(df):
             .reset_index())
     
     df = (df.drop(columns=['scrape_id', 'last_scraped', 'source', 'picture_url', 
-                                        'amenities', 'minimum_nights', 
-                                        'maximum_nights', 'minimum_minimum_nights', 'maximum_minimum_nights',
+                                        'amenities', 'minimum_minimum_nights', 'maximum_minimum_nights',
                                         'minimum_maximum_nights', 'maximum_maximum_nights', 'calendar_last_scraped',
-                                        'index', 'listing_url', 'host_url', 'number_of_reviews_ltm', 'number_of_reviews_l30d'
+                                        'index', 'listing_url', 'number_of_reviews_ltm', 'number_of_reviews_l30d'
                                         ])
                                         .rename(columns={'neighbourhood_cleansed': 'neighbourhood'})
                                         )
@@ -60,10 +59,15 @@ def transform_reviews_silver(df):
     return df
 
 #%%
-def transform_calendar_silver():    
-    df = pd.read_csv(calendar_path)
+def transform_calendar_silver(df):    
     logger.info('Starting airbnb calendar data transforming')
     #Silver layer
+    
+    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+    today = pd.Timestamp(datetime.today()).normalize()
+    date_condition = df['date'] >= today
+    df = df[date_condition]
+    
     df = (df.sort_values(['date'], ascending=False)
             .drop(columns=['adjusted_price'])
             .dropna()
@@ -72,8 +76,6 @@ def transform_calendar_silver():
     
     df['available'] = df['available'].replace({'f': 'No', 't': 'Yes'})
     
-    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
-    df[['minimum_nights', 'maximum_nights']] = df[['minimum_nights', 'maximum_nights']].astype(int)
     
     df['price_USD'] = df['price_USD'].str.replace(r'[$,]', '', regex=True)
     df['price_USD'] = df['price_USD'].astype(float)
@@ -93,6 +95,9 @@ transform_calendar_silver()
 
 #%%
 def transform_calendar_gold(df):
+    
+    df_listings = pd.read_csv(listings_path)
+    
     conversion_rate = get_exchange_rate()
     df['price_BRL'] = (df['price_USD'] * conversion_rate).round(decimals=2)
     df_bookings_price = df.drop_duplicates(subset=['listing_id'], keep='last')
@@ -135,9 +140,8 @@ def transform_calendar_gold(df):
     next_five_days_availability = df_available_bookings[five_days_availability]
     
     next_five_days_availability.to_csv(os.path.join(GOLD_DIR, 'next_five_days_listings_availability'), index=False)
+    
 
-
-#%%
 def transform_airbnb_data_silver():    
     df_listings = pd.read_csv(listings_path)
     df_calendar = pd.read_csv(calendar_path)
@@ -151,39 +155,4 @@ def transform_airbnb_data_silver():
     logger.info('All transforming executed successfully')
     return
 
-#%%
-transform_airbnb_data()
-# #%%
-# today = pd.Timestamp(datetime.today()).normalize()
-# today
 
-# #%%
-# calendar_path = os.path.join(BASE_DIR, 'data', 'airbnb', 'bronze', 'calendar.csv')
-# df_calendar = pd.read_csv(calendar_path)
-# df_calendar
-
-# #%%
-# today = pd.Timestamp(datetime.today()).normalize()
-# df_calendar['date'] = pd.to_datetime(df_calendar['date'])
-# date_range = pd.date_range(today, periods=5)
-
-# next_five_days_availability = df_calendar['date'].isin(date_range)
-
-# #%%
-# next_five_days = df_calendar[next_five_days_availability]
-# next_five_days.reset_index()
-# grouped = next_five_days.groupby(['date'])
-
-# grouped.head()
-
-
-# #%%
-# listings_path = os.path.join(BASE_DIR, 'data', 'airbnb', 'bronze', 'listings.csv')
-# df_listings = pd.read_csv(listings_path)
-# df_listings[['latitude', 'longitude']]
-
-
-#%%
-df = pd.read_csv(calendar_path)
-
-#%%
