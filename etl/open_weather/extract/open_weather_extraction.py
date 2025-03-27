0e4d7ecfd99c8e019ab4a0f9711262c22e057f8b
Vinícius Vicari <vikarih.punk@gmail.com>
2025-03-26 22:59:57 -0300

    adding openweather data treatment into a function

--- etl/open_weather/extract/open_weather_extraction.py
+++ etl/open_weather/extract/open_weather_extraction.py
@@ -6,41 +6,37 @@ import pandas as pd
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
 load_dotenv(os.path.join(BASE_DIR, 'config', '.env'))
-
 API_KEY = os.getenv('OPENWEATHER_API_KEY')
-URL = f'https://api.openweathermap.org/data/2.5/forecast?lat=-22.995080&lon=-43.236040&units=metric&appid={API_KEY}'
+
+def get_weather_info(lat='-22.9035', lon='-43.2096'):
+
+    URL = f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=metric&appid={API_KEY}'
 
 
-response = requests.get(URL)
-data = response.json()
+    response = requests.get(URL)
+    data = response.json()
 
 
-#%%
-neighbourhood = data['city']['name']
+    neighbourhood = data['city']['name']
+    
+    df = pd.DataFrame(data['list'])
+    
+    df_main = pd.json_normalize(df['main'])
+    df_clouds = pd.json_normalize(df['clouds'])
+    df_rain = pd.json_normalize(df['rain']).fillna(0)
+    df_weather = pd.json_normalize(df['weather'].apply(lambda x: x[0]))
 
-#%%
-df = pd.DataFrame(data['list'])
+    df_final = pd.concat([df[['dt_txt']], df_main, df_clouds, df_rain, df_weather[['main']]], axis=1)
 
-#%%
-df_main = pd.json_normalize(df['main'])
-df_clouds = pd.json_normalize(df['clouds'])
-df_rain = pd.json_normalize(df['rain']).fillna(0)
-df_weather = pd.json_normalize(df['weather'].apply(lambda x: x[0]))
+    df_final.rename(columns={
+        'dt_txt': 'date',
+        'all': '%_clouds',
+        'main': 'weather',
+        '3h': '3h_rain'
+    }, inplace=True)
 
-
-#%%
-df_final = pd.concat([df[['dt_txt']], df_main, df_clouds, df_rain, df_weather[['main']]], axis=1)
-
-#%%
-df_final.rename(columns={
-    'dt_txt': 'date',
-    'all': '%_clouds',
-    'main': 'weather',
-    '3h': '3h_rain'
-}, inplace=True)
-
-#%%
-df_final.insert(0, 'id', range(1, len(df_final) + 1))
-df_final.insert(1, 'neighbourhood', neighbourhood)
-df_final.reset_index(drop=True, inplace=True)
-
+    df_final.insert(0, 'id', range(1, len(df_final) + 1))
+    df_final.insert(1, 'neighbourhood', neighbourhood)
+    df_final.reset_index(drop=True, inplace=True)
+    
+    return df_final
