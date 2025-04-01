e08b03218260aec60a2ef32fab31e3f842711c46
Vinícius Vicari <vikarih.punk@gmail.com>
2025-03-31 22:33:33 -0300

    adding column names for airbnb host model

--- etl/airbnb/gold/process_hosts.py
+++ etl/airbnb/gold/process_hosts.py
@@ -1,6 +1,12 @@
+#%%
 import os
 from utils.config import SILVER_DIR, GOLD_DIR
+import pandas as pd
 
 
-def transform_hosts_gold():
+hosts_path = os.path.join(SILVER_DIR, 'hosts_clean.csv')
+df = pd.read_csv(hosts_path)
+
+#%%
+df.info()
     
--- etl/airbnb/silver/transform_listings.py
+++ etl/airbnb/silver/transform_listings.py
@@ -101,8 +101,8 @@ def transform_listings_silver():
                     'has_availability': 'string',
                     'first_review': 'datetime64[ns]',
                     'last_review': 'datetime64[ns]',
-                    'instant_bookable': 'string',
-            }))
+                    'instant_bookable': 'string',}))
+    
     df['first_review_filled'] = df['first_review'].fillna(pd.NaT)
     df['never_reviewd'] = df['first_review'].isna().astype(int)
     df = df.replace({
@@ -119,7 +119,3 @@ def transform_listings_silver():
     logger.info('Airbnb listings data transformed and saved successfully!')
 
     return
-
-transform_listings_silver()
-
- 
